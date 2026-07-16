68d83e22e8c0cf995cb6df29c08500fd777e9914
AdarshSingh-tech <adarshsingh200521@gmail.com>
2026-07-16 12:11:40 +0530

    Correct sum calculation and error handling in 1st.c
    Fix errors in sum calculation and output formatting.

--- day1/1st.c
+++ day1/1st.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum;
+    
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    if(n<=0){
+printf("error");
+return 1;
+for(int i=1;i<=n;i++)
+{
+sum+=i;
+}
+printf("the sum of first n nutral number ,n,sum);
+  return 0;
+  }
+  }
 
