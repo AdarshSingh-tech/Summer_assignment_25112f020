a43033dd54deec8be4b862824f22338d8773afc2
AdarshSingh-tech <adarshsingh200521@gmail.com>
2026-07-16 22:04:20 +0530

    Add files via upload

--- day1/4th.c
+++ day1/4th.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+    int count = 0;
+
+    printf("Enter a number: ");
+    if (scanf("%d", &number) != 1) {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    if (number == 0) {
+        count = 1;
+    } else {
+        if (number < 0) {
+            number = -number;
+        }
+        while (number > 0) {
+            number /= 10;
+            count++;
+        }
+    }
+
+    printf("Number of digits = %d\n", count);
+    return 0;
+}
 
