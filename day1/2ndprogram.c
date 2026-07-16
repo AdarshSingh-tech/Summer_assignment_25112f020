001cde649cbf4c9746d879c2632c4f05b8c068fc
AdarshSingh-tech <adarshsingh200521@gmail.com>
2026-07-16 22:00:27 +0530

    Add files via upload

--- day1/2ndprogram.c
+++ day1/2ndprogram.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number, i;
+
+    printf("Enter a number: ");
+    if (scanf("%d", &number) != 1) {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    printf("Multiplication table for %d:\n", number);
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", number, i, number * i);
+    }
+
+    return 0;
+}
 
