9c7d48bff785b670af706696c11aa97ec411dbf9
AdarshSingh-tech <adarshsingh200521@gmail.com>
2026-07-16 22:02:52 +0530

    Add files via upload

--- day1/3rd.c
+++ day1/3rd.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+    unsigned long long factorial = 1;
+    int i;
+
+    printf("Enter a non-negative integer: ");
+    if (scanf("%d", &number) != 1) {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    if (number < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+        return 1;
+    }
+
+    for (i = 1; i <= number; i++) {
+        factorial *= i;
+    }
+
+    printf("Factorial of %d = %llu\n", number, factorial);
+    return 0;
+}
 
